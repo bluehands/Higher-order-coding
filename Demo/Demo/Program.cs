19c19777b7874718d3b65799ef065403ea560c73
Waldemar Tomme <waldetomme@gmail.com>
2021-02-07 17:59:38 +0100

    Add Demo solution

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HelloWorldNamespace.HelloWorld.OnConsole();
+        }
+    }
+}
---
+++ Demo/SourceGen/HelloWorldGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace SourceGen
+{
+    [Generator]
+    public class HelloWorldGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("HelloWorld.cs", @"
+using System;
+
+namespace HelloWorldNamespace
+{
+    public static class HelloWorld
+    {
+        public static void OnConsole()
+        {
+            Console.WriteLine(""Hello World."");
+        }
+    }
+}
+");
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
