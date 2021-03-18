1b268862d47843f45185834b67e4b1c323b78c4e
WiiPlayer2 <waldetomme@gmail.com>
2021-03-18 20:25:24 +0100

    Post-presentation commit

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DarkLink.AutoNotify;
 
 namespace Demo
 {
@@ -6,7 +7,37 @@ namespace Demo
     {
         private static void Main(string[] args)
         {
-            HelloWorldNamespace.HelloWorld.OnConsole();
+            var obj = new ClassA();
+            obj.FieldA = 2;
+            obj.FieldB = 4;
+
+            var theGoodness = GoodEnum.Good;
+            var theGoodness2 = theGoodness.Match(
+                () => "good",
+                () => "very good",
+                () => "the goodest",
+                () => "the very goodest");
+
+            var schema = new Schema();
+
+            var output = BF.HelloWorld();
         }
+    }
+
+    public enum GoodEnum
+    {
+        Good,
+        VeryGood,
+        TheGoodest,
+        TheVeryGoodest,
+    }
+
+    public partial class ClassA
+    {
+        [AutoNotify]
+        private int fieldA, fieldB;
+
+        [AutoNotify(UsePrivateSetter = true)]
+        private float fieldC;
     }
 }
---
+++ Demo/SourceGen/HelloWorld2Generator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceGen
+{
+    [Generator]
+    public class HelloWorld2Generator : ISourceGenerator, ISyntaxReceiver
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("HelloWorld.cs", @"
+using System;
+public static class HelloWorld
+{
+    public static void Hola() => Console.WriteLine(""Hola!"");
+}
+");
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => this);
+        }
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+        }
+    }
+}
